0a3fee8ba7e9f7b71434752157981a6d681a1f7b
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-13 22:52:33 -0300

    Dependency handover for UINavigationController and UITabBarController

--- Scopes/Common/DISegueCoordinator.swift
+++ Scopes/Common/DISegueCoordinator.swift
@@ -19,13 +19,29 @@ extension DISegueCoordinator {
     func navigate(with safeSegue: SafeDISegue) {
         performSegue(withIdentifier: safeSegue.identifier, sender: safeSegue)
     }
+    
+    fileprivate func possibleTargets(for destination: UIViewController) -> [DITarget] {
+        var availableViewControllers = [destination]
+        
+        if let navigation = destination as? UINavigationController {
+            availableViewControllers.append(contentsOf: navigation.viewControllers)
+        }
+        
+        if let tapBarController = destination as? UITabBarController,
+           let viewControllers = tapBarController.viewControllers {
+            availableViewControllers.append(contentsOf: viewControllers)
+        }
+        
+        return availableViewControllers.compactMap({ $0 as? DITarget })
+    }
 }
 
 extension UIViewController: DISegueCoordinator {
     @objc dynamic private func swizzledPrepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = sender as? SafeDISegue,
-           let target = segue.destination as? DITarget {
-            destination.diContainer?.performInjection(into: target.field)
+        if let safeSegue = sender as? SafeDISegue {
+            possibleTargets(for: segue.destination).forEach() { target in
+                safeSegue.diContainer?.performInjection(into: target.field)
+            }
         }
         swizzledPrepare(for: segue, sender: sender)
     }
