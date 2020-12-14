2eecd404beebb445fad23c49e0d522869446d4ca
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-13 21:38:25 -0300

    Start initial navigation structure

---
+++ Scopes/Start/StartDestination.swift
@@ -0,0 +1,28 @@
+//
+//  StartDestination.swift
+//  Scopes
+//
+//  Created by Ricardo Carvalho on 13/12/20.
+//
+
+import Foundation
+
+extension StructuredSafeDISegue.Origin {
+    static func start(to target: StartDestination) -> Self {
+        Self(target.segue)
+    }
+    
+    struct StartDestination: CompositeSafeDISegue {
+        let segue: SafeDISegue
+    }
+}
+
+extension StructuredSafeDISegue.Origin.StartDestination {
+    static var login: Self {
+        Self(SafeDISegue("startToLogin"))
+    }
+
+    static var main: Self {
+        Self(SafeDISegue("startToMain"))
+    }
+}
