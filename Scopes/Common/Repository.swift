50589d27fca9dd02ed331252e31f292a480455c0
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-17 01:27:32 -0300

    Children repository concept discard

--- Scopes/Common/Repository.swift
+++ Scopes/Common/Repository.swift
@@ -16,20 +16,6 @@ protocol Repository {
     func delete(_ element: Element, completion: (Bool) -> Void)
 }
 
-protocol ChildrenRepository: Repository {
-    associatedtype ParentElement
-    
-    func getParentElement() -> ParentElement
-    func setParentElement(_: ParentElement)
-}
-
-extension ChildrenRepository {
-    var parentElement: ParentElement {
-        get { getParentElement() }
-        set { setParentElement(newValue) }
-    }
-}
-
 struct AnyRepository<E>: Repository {
     private let create: (E, (E) -> Void) -> Void
     private let obtain: (Int, E?, ([E]) -> Void) -> Void
@@ -61,47 +47,5 @@ struct AnyRepository<E>: Repository {
     
     func delete(_ element: E, completion: (Bool) -> Void) {
         delete(element, completion)
-    }
-}
-
-struct AnyChildrenRepository<P, E>: ChildrenRepository {
-    private let anyRepository: AnyRepository<E>
-    private let getAnyParentElement: () -> P
-    private let setAnyParentElement: (P) -> Void
-    
-    init<R: ChildrenRepository>(
-        _ repository: R
-    ) where R.Element == E, R.ParentElement == P {
-        anyRepository = AnyRepository(repository)
-        getAnyParentElement = repository.getParentElement
-        setAnyParentElement = repository.setParentElement
-    }
-    
-    func create(new element: E, completion: (E) -> Void) {
-        anyRepository.create(new: element, completion: completion)
-    }
-    
-    func obtain(
-        first elements: Int = 10,
-        after element: Element? = nil,
-        completion: ([Element]) -> Void
-    ) {
-        anyRepository.obtain(first: elements, after: element, completion: completion)
-    }
-    
-    func update(_ element: E, completion: (Bool) -> Void) {
-        anyRepository.update(element, completion: completion)
-    }
-    
-    func delete(_ element: E, completion: (Bool) -> Void) {
-        anyRepository.delete(element, completion: completion)
-    }
-    
-    func getParentElement() -> P {
-        getAnyParentElement()
-    }
-    
-    func setParentElement(_ element: P) {
-        setAnyParentElement(element)
     }
 }
