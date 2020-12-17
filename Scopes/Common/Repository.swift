f2bfc3e46fb22b8f844ec4c9477123729d4cfff2
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-17 01:46:51 -0300

    Repository refactoring to use Result in completions

--- Scopes/Common/Repository.swift
+++ Scopes/Common/Repository.swift
@@ -7,20 +7,29 @@
 
 import Foundation
 
+typealias ValueResult<T> = Result<T, Error>
+typealias EmptyResult = Result<Void, Error>
+
 protocol Repository {
     associatedtype Element
     
-    func create(new element: Element, completion: (Element) -> Void)
-    func obtain(first elements: Int, after element: Element?, completion: ([Element]) -> Void)
-    func update(_ element: Element, completion: (Bool) -> Void)
-    func delete(_ element: Element, completion: (Bool) -> Void)
+    func create(new element: Element,
+                completion: (ValueResult<Element>) -> Void)
+    
+    func obtain(first elements: Int,
+                after element: Element?,
+                completion: (ValueResult<[Element]>) -> Void)
+    
+    func update(_ element: Element, completion: (EmptyResult) -> Void)
+    
+    func delete(_ element: Element, completion: (EmptyResult) -> Void)
 }
 
 struct AnyRepository<E>: Repository {
-    private let create: (E, (E) -> Void) -> Void
-    private let obtain: (Int, E?, ([E]) -> Void) -> Void
-    private let update: (E, (Bool) -> Void) -> Void
-    private let delete: (E, (Bool) -> Void) -> Void
+    private let create: (E, (ValueResult<E>) -> Void) -> Void
+    private let obtain: (Int, E?, (ValueResult<[E]>) -> Void) -> Void
+    private let update: (E, (EmptyResult) -> Void) -> Void
+    private let delete: (E, (EmptyResult) -> Void) -> Void
     
     init<R: Repository>(_ repository: R) where R.Element == E {
         create = repository.create
@@ -29,23 +38,23 @@ struct AnyRepository<E>: Repository {
         delete = repository.delete
     }
     
-    func create(new element: E, completion: (E) -> Void) {
+    func create(new element: E, completion: (ValueResult<E>) -> Void) {
         create(element, completion)
     }
     
     func obtain(
         first elements: Int = 10,
-        after element: Element? = nil,
-        completion: ([Element]) -> Void
+        after element: E? = nil,
+        completion: (ValueResult<[E]>) -> Void
     ) {
         obtain(elements, element, completion)
     }
     
-    func update(_ element: E, completion: (Bool) -> Void) {
+    func update(_ element: E, completion: (EmptyResult) -> Void) {
         update(element, completion)
     }
     
-    func delete(_ element: E, completion: (Bool) -> Void) {
+    func delete(_ element: E, completion: (EmptyResult) -> Void) {
         delete(element, completion)
     }
 }
