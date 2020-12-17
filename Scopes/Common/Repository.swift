254e3c00d9e2b06c90889c0a3d45199a8e212bc0
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-17 06:21:32 -0300

    Repository simplification

--- Scopes/Common/Repository.swift
+++ Scopes/Common/Repository.swift
@@ -10,15 +10,15 @@ import Foundation
 typealias ValueResult<T> = Result<T, Error>
 typealias EmptyResult = Result<Void, Error>
 
-struct IdentifiableItem<T, I: Hashable>: Identifiable {
-    let id: I
+struct IdentifiableItem<T>: Identifiable {
+    let id: String
+    let path: String
     let item: T
 }
 
 protocol Repository {
     associatedtype Element
-    associatedtype Identifier: Hashable
-    typealias IdentifiableElement = IdentifiableItem<Element, Identifier>
+    typealias IdentifiableElement = IdentifiableItem<Element>
     
     func create(new element: Element,
                 completion: (ValueResult<IdentifiableElement>) -> Void)
@@ -32,15 +32,15 @@ protocol Repository {
     func delete(_ element: IdentifiableElement, completion: (EmptyResult) -> Void)
 }
 
-struct AnyRepository<E, I: Hashable>: Repository {
-    typealias IdentifiableElement = IdentifiableItem<E, I>
+struct AnyRepository<E>: Repository {
+    typealias IdentifiableElement = IdentifiableItem<E>
     
     private let create: (E, (ValueResult<IdentifiableElement>) -> Void) -> Void
     private let obtain: (Int, IdentifiableElement?, (ValueResult<[IdentifiableElement]>) -> Void) -> Void
     private let update: (IdentifiableElement, (EmptyResult) -> Void) -> Void
     private let delete: (IdentifiableElement, (EmptyResult) -> Void) -> Void
     
-    init<R: Repository>(_ repository: R) where R.Element == E, R.Identifier == I {
+    init<R: Repository>(_ repository: R) where R.Element == E {
         create = repository.create
         obtain = repository.obtain
         update = repository.update
