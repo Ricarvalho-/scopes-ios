306fce0745dd44a92f77fcba0f75f56a8a2d2f3c
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-21 23:18:53 -0300

    Scopes ViewController content and navigation implementation

--- Scopes/Main/Scopes/ScopesViewController.swift
+++ Scopes/Main/Scopes/ScopesViewController.swift
@@ -11,4 +11,37 @@ class ScopesViewController: UITableViewController, TypedDITarget {
     typealias Dependency = FutureRepository<Scope>
     
     let field = DIField<Any>()
+    
+    private var contentManager: ContentScreenManager<Scope>? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let repository = safeDependency {
+            contentManager = ContentScreenManager(
+                repository: repository,
+                tableViewController: self,
+                delegate: self
+            )
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        contentManager?.refreshContent()
+    }
+    
+    @IBAction func refreshContent() {
+        contentManager?.refreshContent()
+    }
+}
+
+extension ScopesViewController: ContentScreenManagerDelegate {
+    func update(cell: UITableViewCell, for item: Scope) {
+        cell.textLabel?.text = item.title
+    }
+    
+    func navigate(to item: IdentifiableItem<Scope>) {
+        let goalsRepository = FirestoreGoalsRepository(parent: item)
+        navigate(.from(.scopes(to: .goals(AnyRepository(goalsRepository)))))
+    }
 }
