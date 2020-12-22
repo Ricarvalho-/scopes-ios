5a2c388c10bd993e94caa0954ca563065127c491
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-21 23:23:21 -0300

    Initial navigation to scopes while auth isn't implemented

--- Scopes/Start/StartViewController.swift
+++ Scopes/Start/StartViewController.swift
@@ -7,4 +7,9 @@
 
 import UIKit
 
-class StartViewController: UIViewController {}
+class StartViewController: UIViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigate(.from(.start(to: .main(AnyRepository(FirestoreScopesRepository())))))
+    }
+}
