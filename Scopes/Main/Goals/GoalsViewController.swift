c50ba645b91196cd81e443fa669ceaec303ea3b9
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-12-22 01:02:20 -0300

    Goals ViewController content and navigation implementation

--- Scopes/Main/Goals/GoalsViewController.swift
+++ Scopes/Main/Goals/GoalsViewController.swift
@@ -11,4 +11,44 @@ class GoalsViewController: UITableViewController, TypedDITarget {
     typealias Dependency = FutureRepository<Goal>
     
     let field = DIField<Any>()
+    
+    private var contentManager: ContentScreenManager<Goal>? = nil
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let repository = safeDependency {
+            contentManager = ContentScreenManager(
+                repository: repository,
+                tableViewController: self,
+                delegate: self
+            )
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        contentManager?.refreshContent()
+    }
+    
+    @IBAction func refreshContent() {
+        contentManager?.refreshContent()
+    }
+}
+
+extension GoalsViewController: ContentScreenManagerDelegate {
+    func update(cell: UITableViewCell, for item: Goal) {
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = dateFormatter.string(from: item.dueDate)
+    }
+    
+    func didSelect(_ item: IdentifiableItem<Goal>) {
+        let tasksRepository = FirestoreTasksRepository(parent: item)
+        navigate(.from(.goals(to: .tasks(AnyRepository(tasksRepository)))))
+    }
 }
